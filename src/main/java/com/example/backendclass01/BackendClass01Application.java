4b172fccf23047dec92f29e6e1cea28d133b6efc
Mohammad Anishur Rahman <60766792+anishurrahman@users.noreply.github.com>
2020-04-05 17:45:53 -0700

    initial commit

---
+++ src/main/java/com/example/backendclass01/BackendClass01Application.java
@@ -0,0 +1,13 @@
+package com.example.backendclass01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendClass01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendClass01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendclass01/BackendClass01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendclass01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendClass01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
